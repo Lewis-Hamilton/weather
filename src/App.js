935bdde9bcb84b5ee952dd188c0189f9f9b3034b
Lewis <hamiltonjlewis@gmail.com>
2021-08-26 13:54:58 -0700

    working api call that I dont understand

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 export default function App() {
-  fetch("https://dad-jokes.p.rapidapi.com/random/joke/png", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
-      "x-rapidapi-key": "7076ad1595msh127766a5ba6d357p18e999jsn3e77642477f6",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-
   const call = () => {
-    console.log("hello");
+    fetch("https://dad-jokes.p.rapidapi.com/random/joke/png", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+        "x-rapidapi-key": "7076ad1595msh127766a5ba6d357p18e999jsn3e77642477f6",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.body.setup);
+        console.log(response.body.punchline);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
   return (
     <div className="App">
