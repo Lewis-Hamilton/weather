5d13af4e26586422db54c86c2488fd0c274bd4e7
Lewis <hamiltonjlewis@gmail.com>
2021-09-06 18:38:19 -0700

    added seperate settings component

--- src/components/settings.js
+++ src/components/settings.js
@@ -7,57 +7,17 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Switch from "@material-ui/core/Switch";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import Modal from "./modal";
-import Settings from "./settings";
 
-export default function Home() {
+export default function Settings() {
   const [open, setOpen] = useState(false);
   const [unit, setUnit] = useState("imperial");
   const [city, setCity] = useState("Unkown");
-  const [temp, setTemp] = useState("");
-  const [feel, setFeel] = useState("");
   const [check, setCheck] = useState(false);
-  const options = {
-    method: "GET",
-    url: "https://community-open-weather-map.p.rapidapi.com/weather",
-    params: {
-      q: city,
-      units: unit,
-    },
-    headers: {
-      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-      "x-rapidapi-key": process.env.REACT_APP_WEATHER_API_KEY,
-    },
-  };
-
-  const call = () => {
-    if (city === "Unkown" || "") {
-      setTemp("Enter a location Dumbass");
-    } else {
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data);
-          console.log(response.data.main.temp);
-          setTemp(response.data.main.temp);
-          setFeel(response.data.main.feels_like);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    }
-  };
 
   useEffect(() => {
     unitChange();
   });
-
-  useEffect(() => {
-    call();
-  }, []);
 
   const unitChange = () => {
     if (check === true) {
@@ -69,9 +29,7 @@ export default function Home() {
 
   return (
     <>
-      <Settings />
-      <Modal />
-      {/* <IconButton onClick={() => setOpen(true)}>
+      <IconButton onClick={() => setOpen(true)}>
         <SettingsIcon />
       </IconButton>
       <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
@@ -96,13 +54,7 @@ export default function Home() {
             ></TextField>
           </ListItem>
         </List>
-      </Drawer> */}
-      <Button variant="contained" color="primary" onClick={() => call()}>
-        Call
-      </Button>
-      <Typography variant="h1">Temperature: {temp}</Typography>
-      <Typography variant="h2">Feels Like: {feel}</Typography>
-      <Typography variatn="h5">Current Location: {city}</Typography>
+      </Drawer>
     </>
   );
 }
