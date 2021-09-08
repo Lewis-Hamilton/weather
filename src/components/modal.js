a9f8a8e5061ac71ae93a26da3a4678e18bf0a4e4
Lewis <hamiltonjlewis@gmail.com>
2021-09-07 19:55:48 -0700

    added text field to modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,11 +3,14 @@ import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import { useDispatch } from "react-redux";
+import { changeLocation } from "../redux/reducers/locationReducer";
 
 export default function Modal() {
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setOpen(true);
@@ -25,10 +28,11 @@ export default function Modal() {
           {"Please Enter Your Location"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
+          <TextField
+            label="Location"
+            variant="outlined"
+            onChange={(e) => dispatch(changeLocation(e.target.value))}
+          ></TextField>
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpen(false)} color="primary" autoFocus>
