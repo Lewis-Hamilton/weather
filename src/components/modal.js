aff9d3f750590eb2434ae43ab74c097f74f4e6e1
Lewis <hamiltonjlewis@gmail.com>
2021-09-08 08:51:04 -0700

    moved settings

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,14 +5,16 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeLocation } from "../redux/reducers/locationReducer";
 
 export default function Modal() {
   const [open, setOpen] = useState(false);
+  const city = useSelector((state) => state.locationReducer.location);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // if (city !== "") setOpen(true);
     setOpen(true);
   }, []);
 
