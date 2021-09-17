238bf30e31ec6cfc257a90abfbe1c43fbc280838
Lewis <hamiltonjlewis@gmail.com>
2021-09-17 15:09:12 -0700

    moved style to index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React from "react";
 import Home from "./components/home";
-import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./theme";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     background: "linear-gradient(315deg, #2a2a72 0%, #009ffd 74%)",
-//     height: "100vh",
-//     color: "white",
-//   },
-// }));
 
 export default function App() {
-  // const classes = useStyles();
-  return (
-    <ThemeProvider theme={theme}>
-      {/* <Box className={classes.root}> */}
-      <Home />
-      {/* </Box> */}
-    </ThemeProvider>
-  );
+  return <Home />;
 }
--- src/theme.js
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@material-ui/core";
-
-const theme = createTheme({
-  backgroundColor: "black",
-  color: "white",
-});
-
-export default theme;
